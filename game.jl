e653b976dd0d6925867552e01c8b84945a1aca7e
Sam Fuchs <samcfuchs@gmail.com>
2021-10-12 16:43:37 -0400

    Basic gameplay works

---
+++ game.jl
@@ -0,0 +1,42 @@
+module Minesweeper
+
+using Random: shuffle
+using PaddedViews: PaddedView
+
+export make_board, request, Index
+
+Minefield = Array{Bool}
+Index = CartesianIndex{2}
+
+# Make a board with random mine positions
+function make_board(h::Int,w::Int,n=0)::Minefield
+    @assert n <= h * w
+
+    b = zeros(Bool,(h,w))
+    b[1:n] .= 1.0
+    shuffle(b)
+end
+
+function neighbors(board::Minefield, ix::Index)::Int
+    filt = ones(3,3)
+    filt[2,2] = 0
+    one = Index(1,1)
+
+    pad = PaddedView( 0, board, (0:size(board,1)+1, 0:size(board,2)+1) )
+    neighborhood = pad[ ix-one : ix+one ]
+
+    sum(neighborhood .* filt)
+end
+
+function request(board::Minefield, ix::Tuple)::Int
+    ix = Index(ix)
+    if board[ix]
+        # Game over
+        return -1
+    else
+        return neighbors(board, ix)
+    end
+end
+
+
+end
---
+++ main.jl
@@ -0,0 +1,94 @@
+include("./game.jl")
+
+H = 16
+W = 16
+N = 20
+
+#@enum Tile Flag Unk
+
+Tile = Union{Int, Char}
+
+
+board = Minesweeper.make_board(H, W, N)
+
+info = Array{Tile, 2}(undef, H, W)
+info .= '?'
+
+function printinfo(arr)
+    r,c = size(arr)
+
+    println(" "^3, join([lpad(i, 3) for i in 1:c]))
+    println(" "^5, "   "^c)
+    #println("    ", join(1:c, " "))
+    for i in 1:r
+        print(lpad(i, 2), " - ")
+        print(join(arr[i,:], "  "))
+        println()
+    end
+end
+
+function explorezero(r,c)
+    k = info[r,c]
+    if k == '?'
+        ct = Minesweeper.request(board, (r,c))
+        if ct != 0
+            info[r,c] = ct
+            return
+        end
+        
+        info[r,c] = ' '
+
+        for i in max(1, r-1):min(H, r+1)
+            for j in max(1, c-1):min(W, c+1)
+                explorezero(i,j)
+            end
+        end
+    end
+end
+    
+
+
+while true
+    printinfo(info)
+    println()
+
+    input = split(readline())
+    command = input[1]
+
+    if command == "quit"
+        break
+    end
+
+    r = parse(Int, input[2])
+    c = parse(Int, input[3])
+
+    if command == "flag"
+        info[r,c] = 'X'
+    end
+
+    if command == "unflag"
+        info[r,c] = '?'
+    end
+
+    if command == "try"
+        if info[r,c] == 'X'
+            println("That tile is flagged!")
+            continue
+        end
+
+        result = Minesweeper.request(board, (r,c))
+
+        if result == -1
+            println("You hit a mine")
+            break
+        end
+
+        if result == 0
+            explorezero(r,c)
+        else
+            info[r,c] = result
+        end
+    end
+
+end
+
